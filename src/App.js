71f1e660d383b69df03886a167eeaeda1f814b36
DanikMonday <daacostaf@unal.edu.co>
2023-02-07 12:07:53 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Form } from './components/Form';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/*Fea la que lo lea 😂 */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Form/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        <header>
+            <div className='cont-icon'>
+                
+            </div>
+        </header>
+    </div>
+  )
+}
