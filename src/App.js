99bbb136906a33ce8edc1388aa40ba0ff7c6d1bb
DanikMonday <daacostaf@unal.edu.co>
2023-02-07 12:54:23 -0500

    Aside User

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
+import { Aside } from './components/Aside';
 import { Form } from './components/Form';
 import { Header } from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Aside/>
       <Form/>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <div>
         <header>
-            <div className='cont-icon'>
+            <div className='cont-logo'>
                 <img className="img-enc" src={require("../images/LogoFundacionSemilla 2.png")} alt="logo"/>
             </div>
         </header>
