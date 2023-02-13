cc0b4f799c4f0049fc2ccd7a7291dd354ca72157
DanikMonday <daacostaf@unal.edu.co>
2023-02-13 18:06:39 -0500

    deleted forms

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import { Aside } from './components/Aside';
 import { Footer } from './components/Footer';
 import { Form } from './components/Form';
-import { FormCondicional } from './components/FormCondicional';
-import { FormNatural } from './components/FormNatural';
 import { Header } from './components/Header';
 import { Session } from './components/Session';
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { FormNatural } from './FormNatural';
+
 
 
 
