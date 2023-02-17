a2f53b3e291374a66bd4bc169c09ef4656354a1e
DanikMonday <daacostaf@unal.edu.co>
2023-02-17 13:06:03 -0500

    merge corre

--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import axios from 'axios';
-let url = "http://localhost:3030";
-=======
-import React from 'react';
 import { Link } from "react-router-dom";
->>>>>>> daniela
+let url = "http://localhost:3030";  
 
-export const Session = () => {
-    
+export const Session = () => { 
+
     const [data, setData] = useState({
         email: " ",
         password: ""
@@ -33,13 +29,8 @@ export const Session = () => {
         <h2>Una propuesta para tiempos mejores</h2>
         <div className='login-admin-cont'>
             
-<<<<<<< HEAD
             <form onSubmit={login} className='form-login'>
                 <div><strong>Administrador</strong></div>                
-=======
-            <div className='form-login'>
-                
->>>>>>> daniela
                 <div className='input'>
                     <label>Usuario</label>
                     <input name='email' type='email' className='pass-input'  onChange={handleInput} value={data.user} required/>
@@ -55,18 +46,11 @@ export const Session = () => {
                     <label>Mostrar Contraseña</label>
                 </div>
                 <div className='login-btn'>
-<<<<<<< HEAD
-                    <button type='submit'>Ingresar</button>
+                   <Link to='/NewUser'><button type='submit'>Ingresar</button></Link>
                 </div>                
             </form>
-=======
-                   <Link to='/NewUser'><button>Ingresar</button></Link>
-                </div>
-            </div>
->>>>>>> daniela
-            
             <div className='img-login-admin'></div>
-        
+          
         </div>
    </div>
   )
