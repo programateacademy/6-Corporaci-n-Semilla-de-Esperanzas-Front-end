1f0b97deecf0ec695e88b6b149db19f8f75d65f2
Óscar O <oscarforjuela@hotmail.com>
2023-02-21 10:40:22 -0500

    New user component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Aside } from './components/Aside';
 import { Footer } from './components/Footer';
 import { Form } from './components/Form';
 import { Header } from './components/Header';
+import NewUser from './components/NewUser';
 import { Session } from './components/Session';
 
 
@@ -13,8 +14,9 @@ function App() {
       {/*<Header/>*/}
       <div className='content'>
         {/*<div className='lateral'><Aside/></div>
-        <div className='central-body'><Session/></div>*/}
-        <Form></Form>
+        <div className='central-body'><Session/></div>
+        <Form></Form>*/}
+        <NewUser/>
       </div>
       <Footer/>
     </div>
